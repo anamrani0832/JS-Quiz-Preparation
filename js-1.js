ef4f9e4b1db57fa3df4417f15d1507fe48df8a68
Anam Rani <Anam Rani>
2022-09-11 14:26:38 +0500

    practice

--- js-1.js
+++ js-1.js
@@ -1,2 +1,3 @@
 // ALret is a keyword to display a message box
-alert("This is my first program");
+alert("This is my first program");    // Semicolon ends the every statement
+alert("Hi There, Anam");
