699223e023809dbd90138c03ceb09d633b5dcfbf
Anam Rani <Anam Rani>
2022-09-11 14:34:20 +0500

    alert chapter is ccompleted

--- js-1.js
+++ js-1.js
@@ -1,3 +1,6 @@
 // ALret is a keyword to display a message box
 alert("This is my first program");    // Semicolon ends the every statement
 alert("Hi There, Anam");
+
+// ******   Alert Exercices is ended *******
+
